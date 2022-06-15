d6bc181b0fa675031ec27c8d95fb100eb8f52152
siki <musyajuaayush@gmail.com>
2022-06-15 10:44:09 +0545

    changes express listening port

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.listen(3333, () => {
+app.listen(3000, () => {
     console.log("Application listening on port 3333!");
 });
